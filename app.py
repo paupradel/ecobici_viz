ad6513146e55f2944d6971e2716d964a8bbd2f28
paupradel <pradel.paulina@ciencias.unam.mx>
2019-10-28 17:46:36 -0600

    add favicon

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
 import dash
 import dash_html_components as html
 
+# Inicio de app en Dash
 app = dash.Dash(__name__)
 
+# Inicio de servidor
 server = app.server
 
+# Personalización del template de html (head, footer, favicon, etc.)
+app.index_string= '''
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,600&display=swap" rel="stylesheet">
+        <meta charset= "UTF-8">
+        <title>Ecobici</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+    </body>
+</html>'''
+
+
+
+#--------------------------------- Layout de la app-------------------------------#
 app.layout = html.Div([html.H1('HOLA PRROS')])
 
 if __name__ == '__main__':
