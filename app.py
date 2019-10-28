afab9fa285233ca61644e3b2ab107dd0c7463553
Paulina Pradel <pradel.paulina@ciencias.unam.mx>
2019-10-27 20:39:37 -0600

    variable de servidor

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
 
-app= dash.Dash(__name__)
+app = dash.Dash(__name__)
 
-app.layout= html.Div([html.H1('HOLA PRROS')])
+server = app.server
+
+app.layout = html.Div([html.H1('HOLA PRROS')])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
